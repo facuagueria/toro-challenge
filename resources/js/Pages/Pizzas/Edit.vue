c99761542dcca22f4767fdfce1c03fb35f4dd908
facuagueria <facundoagueria1@gmail.com>
2024-04-20 20:07:46 -0300

    feat ✨: Edit Page Pizza

---
+++ resources/js/Pages/Pizzas/Edit.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import {Ingredient} from "@/types/Ingredient";
+import BaseLayout from "@/Layouts/BaseLayout.vue";
+import EditForm from "@/Components/Pizza/EditForm.vue";
+import {Pizza} from "@/types/Pizza";
+
+defineProps<{
+    pizza: Pizza
+    ingredients: Ingredient[]
+}>()
+</script>
+
+<template>
+    <BaseLayout>
+        <template v-slot:title>Edit {{ pizza.name }}</template>
+        <EditForm :pizza="pizza" :ingredients="ingredients"/>
+    </BaseLayout>
+</template>
