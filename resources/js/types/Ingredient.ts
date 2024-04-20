0f2e33f90775c9a7407b6fac35a1516ad2bf9792
facuagueria <facundoagueria1@gmail.com>
2024-04-20 15:27:54 -0300

    feat ✨: types for Ingredient and Pizza

---
+++ resources/js/types/Ingredient.ts
@@ -0,0 +1,7 @@
+export interface Ingredient {
+    id: number;
+    name: string;
+    cost_price: number;
+    created_at: string;
+    updated_at?: string;
+}
---
+++ resources/js/types/Pizza.ts
@@ -0,0 +1,7 @@
+export interface Pizza {
+    id: number;
+    name: string;
+    selling_price: number;
+    created_at: string;
+    updated_at?: string;
+}
