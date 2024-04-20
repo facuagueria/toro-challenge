71e41a83513d07a67e6e1117db41e63deb36e755
facuagueria <facundoagueria1@gmail.com>
2024-04-20 18:54:55 -0300

    feat ✨: ingredients routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Ingredients\IngredientController;
 use App\Http\Controllers\Pizzas\PizzaController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
@@ -32,6 +33,15 @@ Route::group(['prefix' => 'pizzas'], function () {
     Route::get('/{pizza}/edit', [PizzaController::class, 'edit'])->name('pizzas.edit');
     Route::put('/{pizza}', [PizzaController::class, 'update'])->name('pizzas.update');
     Route::delete('/{pizza}', [PizzaController::class, 'destroy'])->name('pizzas.destroy');
+});
+
+Route::group(['prefix' => 'ingredients'], function () {
+    Route::get('/', [IngredientController::class, 'index'])->name('ingredients.index');
+    Route::get('/create', [IngredientController::class, 'create'])->name('ingredients.create');
+    Route::post('/', [IngredientController::class, 'store'])->name('ingredients.store');
+    Route::get('/{ingredient}/edit', [IngredientController::class, 'edit'])->name('ingredients.edit');
+    Route::put('/{ingredient}', [IngredientController::class, 'update'])->name('ingredients.update');
+    Route::delete('/{ingredient}', [IngredientController::class, 'destroy'])->name('ingredients.destroy');
 });
 
 require __DIR__ . '/auth.php';
