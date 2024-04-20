87e3d14a3f74a9700bc9ffc55b3f2c0df9ca125f
facuagueria <facundoagueria1@gmail.com>
2024-04-20 15:32:07 -0300

    feat ✨: create method to return Create pizza page

--- app/Http/Controllers/Pizzas/PizzaController.php
+++ app/Http/Controllers/Pizzas/PizzaController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers\Pizzas;
 
 use App\Http\Controllers\Controller;
+use App\Services\IngredientService;
 use App\Services\PizzaService;
 use Inertia\Inertia;
 
 class PizzaController extends Controller
 {
-    public function __construct(protected PizzaService $pizzaService)
-    {}
+    public function __construct(protected PizzaService $pizzaService, protected IngredientService $ingredientService)
+    {
+    }
 
     public function index()
     {
@@ -17,6 +19,15 @@ class PizzaController extends Controller
 
         return Inertia::render('Pizzas/Index', [
             'pizzas' => $pizzas
+        ]);
+    }
+
+    public function create()
+    {
+        $ingredients = $this->ingredientService->findAll();
+
+        return Inertia::render('Pizzas/Create', [
+            'ingredients' => $ingredients
         ]);
     }
 }
