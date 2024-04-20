0930a0b580c8f0f7d30dae8caba4da3e669f6392
facuagueria <facundoagueria1@gmail.com>
2024-04-20 15:28:10 -0300

    feat ✨: Create Pizza Page

---
+++ resources/js/Pages/Pizzas/Create.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import {Ingredient} from "@/types/Ingredient";
+import BaseLayout from "@/Layouts/BaseLayout.vue";
+import CreateForm from "@/Components/Pizza/CreateForm.vue";
+
+interface Props {
+    ingredients: Ingredient[];
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+    <BaseLayout>
+        <template v-slot:title>Create your own Pizza</template>
+        <CreateForm :ingredients="ingredients"/>
+    </BaseLayout>
+</template>
