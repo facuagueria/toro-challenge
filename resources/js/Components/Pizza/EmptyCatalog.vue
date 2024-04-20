7f90b3eb87e07d1eba14a8e7aad94a5aa9b45f41
facuagueria <facundoagueria1@gmail.com>
2024-04-20 20:09:46 -0300

    fix 🐛: add create button to Pizza/EmptyCatalog

--- resources/js/Components/Pizza/EmptyCatalog.vue
+++ resources/js/Components/Pizza/EmptyCatalog.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-
+import {Link} from "@inertiajs/vue3";
 import {Button} from "@/Components/ui/button";
+
 </script>
 
 <template>
@@ -9,13 +10,12 @@ import {Button} from "@/Components/ui/button";
             <h3 class="text-2xl font-bold tracking-tight">
                 You have no pizzas
             </h3>
-            <Button class="mt-4">
-                Add Pizza
-            </Button>
+            <Link :href="route('pizzas.create')">
+                <Button class="mt-4">
+                    Create
+                </Button>
+            </Link>
+
         </div>
     </div>
 </template>
-
-<style scoped>
-
-</style>
