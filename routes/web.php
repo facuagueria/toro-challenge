95e1055da8b080593b25071b216c78b3311a4eaa
facuagueria <facundoagueria1@gmail.com>
2024-04-20 23:09:51 -0300

    chore: delete unused routes and do a redirect

--- routes/web.php
+++ routes/web.php
@@ -2,28 +2,10 @@
 
 use App\Http\Controllers\Ingredients\IngredientController;
 use App\Http\Controllers\Pizzas\PizzaController;
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    return redirect()->route('pizzas.index');
 });
 
 Route::group(['prefix' => 'pizzas'], function () {
